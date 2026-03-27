8173f63b07f7e5a8015d96466f77695fb34f0520
Madiyar Makhan <laima20192008@gmail.com>
2026-03-27 13:39:59 +0500

    ads

--- src/Main.java
+++ src/Main.java
@@ -1,95 +1,42 @@
-public class Main{
-//1.
-public static int findMax(int[] arr) {
-    int max = arr[0];
-    for (int i = 1; i < arr.length; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-    }
-    return max;
-}
-//2.
-public static boolean contains(int[] arr, int target) {
-    for (int num : arr) {
-        if (num == target) {
-            return true;
-        }
-    }
-    return false;
-}
-//3.
-public static void printPairs(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-        for (int j = 0; j < arr.length; j++) {
-            System.out.println(arr[i] + ", " + arr[j]);
-        }
-    }
-}
-//4.
-public static void uniquePairs(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-        for (int j = i + 1; j < arr.length; j++) {
-            System.out.println(arr[i] + ", " + arr[j]);
-        }
-    }
-}
-//5.
-public static void bubbleSort(int[] arr) {
-    int n = arr.length;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
-//6.
-public static int binarySearch(int[] arr, int target) {
-    int left = 0, right = arr.length - 1;
+import java.util.Scanner;
 
-    while (left <= right) {
-        int mid = (left + right) / 2;
+public class Main {
 
-        if (arr[mid] == target) return mid;
-        if (arr[mid] < target) left = mid + 1;
-        else right = mid - 1;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        reversePrint(n, sc);
     }
 
-    return -1;
-}
-//7.
-public static void doublingLoop(int n) {
-    for (int i = 1; i < n; i *= 2) {
-        System.out.println(i);
+    //1
+    public static int sumSquares(int n) {
+        if (n == 1) return 1;
+        return n * n + sumSquares(n - 1);
     }
-}
-//8.
-public static void tripleLoop(int[] arr) {
-    int n = arr.length;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < n; k++) {
-                System.out.println(arr[i] + arr[j] + arr[k]);
-            }
-        }
+
+    //2
+    public static int sumArray(int[] arr, int n) {
+        if (n == 0) return 0;
+        return arr[n - 1] + sumArray(arr, n - 1);
     }
-}
-//9.
-public static ArrayList<Integer> buildList(int n) {
-    ArrayList<Integer> list = new ArrayList<>();
-    for (int i = 0; i < n; i++) {
-        list.add(i);
+
+    //3
+    public static int power(int b, int n) {
+        if (n == 0) return 1;
+        return b * power(b, n - 1);
     }
-    return list;
-}
-//10
-public static void insertAtBeginning(ArrayList<Integer> list, int n) {
-    for (int i = 0; i < n; i++) {
-        list.add(0, i);
+
+    public static int powerSum(int b, int n) {
+        if (n == 0) return 1;
+        return power(b, n) + powerSum(b, n - 1);
     }
-}
+
+    //4
+    public static void reversePrint(int n, Scanner sc) {
+        if (n == 0) return;
+        int x = sc.nextInt();
+        reversePrint(n - 1, sc);
+        System.out.print(x + " ");
+    }
 }
